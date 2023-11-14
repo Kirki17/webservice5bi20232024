a712d24fafa4a1582107bf8893d6ce5dd7914234
KircanskiLucrezia <118365571+Kirki17@users.noreply.github.com>
2023-11-14 15:15:46 +0000

    commmit

--- ws-server/app.js
+++ ws-server/app.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const mysql = require('mysql2');
 const cors = require('cors');
-const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt');
+const bodyParser = require('body-parser');
 const usersRouter = require('./users.js')
+const jwt = require('jsonwebtoken');
 const fs = require('fs');
 
 const config = require('./config.js');
@@ -16,63 +17,79 @@ service.use(bodyParser.urlencoded({ extended: false }));
 const parametriConnessioneDB = config.dbParams;
 
 service.get('/', (req, res) => {
-    response.sendFile(__dirname + '/help.html');
+    res.sendFile(__dirname + '/help.html');
 });
 
-server.post('/init', (req, res) => {
+service.post('/init', (req, res) => {
     let secret = req.body.secret;
-    if(secret === config.secret){
+    if (secret === config.secret) {
         //1. Carico il file con lo script
-        const scriptSQL = fs.readFileSync(__dirname + '/script.sql').toString();
-       connesione = mysql.createConnection(parametriConnessioneDB);
-       // 2. Eseguo lo script SQL
-       connesione.query(scriptSQL, (error, dati) =>{
-        if(!error){
-            connesione = mysql.createConnection(parametriConnessioneDB);
-            let querySTR = 'INSERT INTO Users (username, password) VALUES (?, ?)'
-            let password = bcrypt.hashSync(config.secret, config.saltRounds);
-            let nuovoUtente = ['admin', password]
-            parametriConnessioneDB.query(querySTR, nuovoUtente, (error, dati) => {
-
-            })
-            res.send('Database inizializzato correttamente');
-        } else {
-            //visualizzo in console l'errore
-            
-            res.status(500).send(error);
-        }
-       });
+        const scriptSQL = fs.readFileSync(__dirname + '/script.sql','utf8');
+        console.log(__dirname+'/script.sql');
+        connesione = mysql.createConnection(parametriConnessioneDB);
+        // 2. Eseguo lo script SQL
+        connesione.query(scriptSQL, (error, dati) => {
+            if (!error) {
+                connessione = mysql.createConnection(parametriConnessioneDB);
+                let querySTR = 'INSERT INTO Users (username, password) VALUES (?, ?)'
+                let password = bcrypt.hashSync(config.secret, config.saltRounds);
+                let nuovoUtente = ['admin', password]
+                connessione.query(querySTR, nuovoUtente, (error, dati) => {
+                    if(!error){
+                        res.send('Database inizializzato correttamente');
+                    } else {
+                        res.status(500).send(error);
+                    }
+                })
+                res.send('Database inizializzato correttamente');
+            } else {
+                //visualizzo in console l'errore
+                res.status(500).send(error);
+            }
+        });
     }
 })
 
 service.post('/login', (req, res) => {
     let username = req.body.username;
     let password = req.body.password;
-    if(username && password) {
+    if (username && password) {
         const connessione = mysql.createConnection(parametriConnessioneDB);
         let querySTR = 'SELECT * FROM Users WHERE username = ?';
         connessione.query(querySTR, username, (error, dati) => {
-            if(!error){
-                if(dati.length > 0){
-                    let user = dati [0];
+            if (!error) {
+                if (dati.length > 0) {
+                    let user = dati[0];
                     let passwordHash = user.password;
-                    if(bcrypt.compareSync(password, passwordHash)){
+                    if (bcrypt.compareSync(password, passwordHash)) {
                         //creare un token bearer ed inviarlo al client
                         //il token deve contenere:
                         // - username
                         // - data di creazione
                         // - data di scadenza (24 ore)
                         // - ruolo (admin, user)
-                        res.send('OK');
+
+                        let token = {};
+                        token.username = user.username;
+                        token.data_creazione = new Date();
+                        token.data_scadenza = new Date();
+                        token.data_scadenza.setDate(token.data_creazione.getDate() + 1)
+                        token.ruolo = 'admin';
+
+                        let tokenBearer = jwt.sign(token, config.secretPhrase);
+                        res.json({ token: tokenBearer });
                     } else {
                         res.status(401).send('Unauthorized');
                     }
-                }
-                else {
-
+                } else {
+                    res.status(401).send('Unauthorized');
                 }
             }
+            res.status(500).send(error);
         })
+        connessione.end(() => { });
+    } else {
+        express.status(401).send('Unauthorized')
     }
 })
 
--- ws-server/config.js
+++ ws-server/config.js
@@ -10,7 +10,8 @@ const config = {
     saltRounds: 10,
     baseUrls : {
         users: '/users'
-    }
+    },
+    secretPhrase: 'Nel mezzo di cammin di nostra vita mi ritrovai per una selva oscura che la diritta via era smarrita'
 }
 
 module.exports = config;
